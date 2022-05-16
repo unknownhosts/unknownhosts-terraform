4fa5b79f668fec642bafd1e1225ee9cab4b428f4
jordan.kim <jordan.kim@kakaopaysec.com>
2022-05-16 23:04:29 +0900

    modify backend

--- services/mgmt/ec2/main.tf
+++ services/mgmt/ec2/main.tf
@@ -8,5 +8,10 @@ locals {
 terraform {
   backend "s3" {  
     key = "mgmt/ec2/terraform.tfstate"
+    bucket = "prod-lincoln-terraform-state"
+    region = "ap-northeast-2"  
+    dynamodb_table = "prod-lincoln-terraform-state-lock-mgmt-ec2"
+    encrypt = true               
+
   }  
 }
