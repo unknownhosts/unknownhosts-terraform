1a6751f224ed0d46669ade59560358331e85adf2
haksuuuuuu <jnd3368@gmail.com>
2022-06-06 23:13:30 +0900

    refafct: eks

--- dev/base/module/eks/eks-cluster.tf
+++ dev/base/module/eks/eks-cluster.tf
@@ -1,32 +1,3 @@
-resource "aws_iam_role" "eks" {
-	name = "${module.global_variables.this_cluster_name}-role"
-
-	assume_role_policy = <<POLICY
-{
-	"Version": "2012-10-17",
-	"Statement": [
-		{
-			"Effect": "Allow",
-			"Principal": {
-				"Service": "eks.amazonaws.com"
-			},
-			"Action": "sts:AssumeRole"
-		}
-	]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster" {
-	policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-	role       = aws_iam_role.eks.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_vpc_controller" {
-	policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-	role       = aws_iam_role.eks.name
-}
-
 resource "aws_eks_cluster" "this" {
 	name     = "${module.global_variables.this_cluster_name}"
 	role_arn = aws_iam_role.eks.arn
