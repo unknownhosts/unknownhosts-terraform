86081584587ccb81c1af30e69d17ebf4dd3917ba
jordan.kim <jordan.kim@kakaopaysec.com>
2022-06-10 15:16:55 +0900

    jordan_terragrunt_script_add

--- _scripts/atlantis/docker_run.sh
+++ _scripts/atlantis/docker_run.sh
@@ -1,5 +1,5 @@
 docker run -d -p 80:4141 custom-atlantis:latest server \
 --gh-user=kimdragon50 \
---gh-token="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" \
+--gh-token="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" \
 --repo-allowlist='*' \
 --repo-config=/usr/local/bin/repos.yaml
